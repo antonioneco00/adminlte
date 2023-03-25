1fb2a51432f5688a8e4c3d66fea671e31a760b62
LAPTOP-OOQ220T8\anton <anevado@ptvtelecom.com>
2023-03-25 12:07:21 +0100

    Modal eventos

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -25968,14 +25968,40 @@ $(window).on("load", function () {
 
     $.ajax({
         url: "http://localhost:8000/api/xlsx-json",
-        success: (data) =>
+        success: (data) => {
             data.map((event) => {
                 fullCalendar.addEvent({
+                    className: "btnEvent",
                     title: event.Titulo,
                     start: event.Inicio,
                     end: event.Fin,
+                    extendedProps: {
+                        type: event.Tipo,
+                    },
                 });
-            }),
+            });
+
+            $(".btnEvent").attr({
+                "data-toggle": "modal",
+                "data-target": "#eventModal",
+            });
+        },
+    });
+
+    $(document).on("click", ".btnEvent", function () {
+        const title = $(this).find(".fc-event-title").html();
+        const selectedEvent = fullCalendar
+            .getEvents()
+            .find((event) => event.title === title);
+
+        const start = selectedEvent.start;
+        const end = selectedEvent.end;
+        const type = selectedEvent.extendedProps.type;
+
+        $('#eventTitle').html(title)
+        $('#eventStart').html(start)
+        $('#eventEnd').html(end)
+        $('#eventType').html(type)
     });
 
     fullCalendar.render();
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,36 @@
 @stop
 
 @section('content')
+    <!-- Modal -->
+    <div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Detalles del evento</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <h2>Título</h2>
+                    <p id="eventTitle"></p>
+
+                    <h2>Fecha de inicio</h2>
+                    <p id="eventStart"></p>
+
+                    <h2>Fecha final</h2>
+                    <p id="eventEnd"></p>
+
+                    <h2>Tipo de evento</h2>
+                    <p id="eventType"></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <p>Welcome to this beautiful admin panel.</p>
 
     <div id="calendar"></div>
